c6c7e41cd7ec090fbae0ed8f763aaf7def5fa6ba
Dao Truong Anh <truongandao16@gmail.com>
2025-12-31 02:19:36 +0700

    upd mô tả db

--- src/main/java/com/bluemoon/app/models/KhoanThuModel.java
+++ src/main/java/com/bluemoon/app/models/KhoanThuModel.java
@@ -1,7 +1,7 @@
 package com.bluemoon.app.models;
 
 public class KhoanThuModel {
-    private int id;
+    private int maKhoanThu;
     private String tenKhoanThu;
     private double soTien;
     private int loaiKhoanThu;
